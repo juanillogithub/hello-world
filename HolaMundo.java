cb9a75e6c1c8ab67c2f337b69811d7507840882a
juanillogithub <39773957+juanillogithub@users.noreply.github.com>
2018-05-30 19:28:27 +0200

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author juanillo
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+        for(int contador=10;contador>0;contador--){
+            System.out.println("El valor de contador es "+contador);
+    }
+    }
+    
+}
